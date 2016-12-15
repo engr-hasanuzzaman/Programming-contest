0c326468eee3e2f2d22193faa39cf804ee134b2e
Hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2016-12-15 12:57:23 +0600

    36 valid sudoku on going file

---
+++ ruby/36. Valid Sudoku.rb
@@ -0,0 +1,13 @@
+# @param {Character[][]} board
+# @return {Boolean}
+# 9*9 size board.
+# There are just 3 rules to Sudoku.
+# 1. Each row must have the numbers 1-9 occuring just once.
+# 2. Each column must have the numbers 1-9 occuring just once.
+# And the numbers 1-9 must occur just once in each of the 9 sub-boxes of the grid.
+def is_valid_sudoku(board)
+    # check each row & column
+end
+
+# check duplicate value in array
+def 

9effbc8b5eba87dce82d431c70340a4aa85443ba
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2019-08-18 14:39:16 +0600

    aSolve new hackerrank problem

---
+++ hackerrank-solutions/Breaking the Records.rb
@@ -0,0 +1,38 @@
+#!/bin/ruby
+
+require 'json'
+require 'stringio'
+
+# Complete the breakingRecords function below.
+def breakingRecords(scores)
+    low, high = scores.first, scores.first
+    size = scores.size
+    high_count = 0
+    low_count = 0
+    # puts "--size #{size}"
+    (1...size).each do |i|
+    #   puts "--i #{i}"
+      num = scores[i]
+      if num < low
+        low_count += 1
+        low = num
+      elsif num > high
+        high_count += 1
+        high = num    
+      end     
+    end
+    [high_count, low_count]
+end
+
+fptr = File.open(ENV['OUTPUT_PATH'], 'w')
+
+n = STDIN.gets.to_i
+
+scores = STDIN.gets.rstrip.split(' ').map(&:to_i)
+
+result = breakingRecords scores
+
+fptr.write result.join " "
+fptr.write "\n"
+
+fptr.close()

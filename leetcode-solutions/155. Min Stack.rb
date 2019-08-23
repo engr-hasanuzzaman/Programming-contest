33f548eed266a05c731285ffefc7ed12fc29daac
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2019-08-24 00:24:29 +0600

    Add new leetcode solution

---
+++ leetcode-solutions/155. Min Stack.rb
@@ -0,0 +1,58 @@
+# https://leetcode.com/problems/min-stack/
+
+class MinStack
+
+  =begin
+      initialize your data structure here.
+  =end
+      def initialize()
+          @stack = []
+      end
+  
+  
+  =begin
+      :type x: Integer
+      :rtype: Void
+  =end
+      def push(x)
+         @stack << x
+          if !@min
+              @min = x
+          elsif @min > x
+              @min = x
+          end
+      end
+  
+  
+  =begin
+      :rtype: Void
+  =end
+      def pop()
+        @stack.pop  
+      end
+  
+  
+  =begin
+      :rtype: Integer
+  =end
+      def top()
+          @stack[-1]
+      end
+  
+  
+  =begin
+      :rtype: Integer
+  =end
+      def get_min()
+          @stack.min
+      end
+  
+  
+  end
+  
+  # Your MinStack object will be instantiated and called as such:
+  # obj = MinStack.new()
+  # obj.push(x)
+  # obj.pop()
+  # param_3 = obj.top()
+  # param_4 = obj.get_min()

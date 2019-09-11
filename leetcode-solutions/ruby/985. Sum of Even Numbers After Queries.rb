4418f9845df5b22e5eece7878ecd21f96e22d4e9
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2019-09-11 23:59:23 +0600

    update solution 985

--- leetcode-solutions/ruby/985. Sum of Even Numbers After Queries.rb
+++ leetcode-solutions/ruby/985. Sum of Even Numbers After Queries.rb
@@ -5,4 +5,15 @@
 # @return {Integer[]}
 def sum_even_after_queries(a, queries)
   even_sum = a.select(&:even?).sum
+  answer = []
+  queries.each do |q|
+      v, i = q
+      if a[i].even?
+          even_sum += v
+      end
+      
+      answer << even_sum
+  end
+  
+  answer
 end

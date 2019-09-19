f15fd1bf2fb3b9e29fd0f1b49bb73a29011522e8
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2019-09-19 13:06:32 +0600

    first F# program

---
+++ f#/hellow.fs
@@ -0,0 +1,12 @@
+open System
+
+let hello() = 
+  printf "Enter your name : "
+
+  let name = Console.ReadLine()
+  printfn "Hello %s" name
+
+
+hello()
+printfn("this is sumon")
+Console.ReadKey() |> ignore

dd3edb23366f285c23e55f1e71a15c119e0747c8
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2019-10-20 23:48:54 +0600

    Solve leetcode problem 1185

---
+++ leetcode-solutions/ruby/1185. Day of the Week.rb
@@ -0,0 +1,8 @@
+require 'time'
+# @param {Integer} day
+# @param {Integer} month
+# @param {Integer} year
+# @return {String}
+def day_of_the_week(day, month, year)
+    Date.new(year,month,day).strftime("%A") 
+end

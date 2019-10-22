bbc0cb37a1468abead25bea87b0f2b6d908bc6ce
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2019-10-22 23:52:40 +0600

    Start solving leetcode problem 788

---
+++ leetcode-solutions/ruby/788. Rotated Digits.rb
@@ -0,0 +1,22 @@
+# @param {Integer} n
+# @return {Integer}
+def rotated_digits(n)
+  counter = 0
+  1.upto(n) do |num|
+      counter +=1 if good_number?(num)
+  end
+  counter
+end
+
+def good_number?(n)
+  good_numbers = ['2', '5', '6', '9']
+  s_n = n.to_s
+  puts "-s_n #{s_n}"
+  good_numbers.each do |n|
+      s_n.delete(n.to_s)
+  end
+  puts "---#{s_n}"
+ !s_n.size.zero?
+end
+
+rotated_digits(10)

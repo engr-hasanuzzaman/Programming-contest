5634c7bcf1e787463f58e5d16ae90eeec6dc12a3
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2019-11-01 20:14:41 +0600

    Start solving 1217

---
+++ leetcode-solutions/ruby/1217. Play with Chips.rb
@@ -0,0 +1,19 @@
+  # https://leetcode.com/problems/play-with-chips/
+
+  # @param {Integer[]} chips
+# @return {Integer}
+def min_cost_to_move_chips(chips)
+  chip_counter = Hash.new(0)
+  chips.each do |i|
+      chip_counter[i] += 1
+  end
+  
+  counter = 0
+  m_index = chip_counter.sort_by{|k, v| -v}.first[0]
+  chips.each do |i|
+      if (m_index - i).abs % 2 == 1
+          counter += 1
+      end
+  end
+  counter
+end

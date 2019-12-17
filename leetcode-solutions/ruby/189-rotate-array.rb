eaab6884e2ccd4598647f0634a2fdd6bf9d464bf
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2019-12-17 11:40:21 +0600

    Implement new solution with extra memory of leetcode 189

--- leetcode-solutions/ruby/189-rotate-array.rb
+++ leetcode-solutions/ruby/189-rotate-array.rb
@@ -10,3 +10,22 @@ def rotate(nums, k)
     # use ruby array rotate method that defaultly rotate left
     nums.rotate!(-actual_rotation)
 end
+
+# rotate using extra array without builtin method
+# @param {Integer[]} nums
+# @param {Integer} k
+# @return {Void} Do not return anything, modify nums in-place instead.
+def rotate(nums, k)
+  return nums if k % nums.size == 0
+  
+  # create new array and fill with result array element
+  temp_a = []
+  nums.each_with_index do |n, i|
+      temp_a[(i+k)%nums.size] = nums[i]
+  end
+  
+  # fill answer array with new values
+  nums.size.times do |i|
+      nums[i] = temp_a[i]
+  end
+end

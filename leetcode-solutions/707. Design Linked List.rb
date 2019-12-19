ccefee892ab842e6c0117067a1d22fc5475aae48
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2019-12-19 13:00:02 +0600

    Implement doubly link list

--- leetcode-solutions/707. Design Linked List.rb
+++ leetcode-solutions/707. Design Linked List.rb
@@ -146,10 +146,184 @@ class MyLinkedList
 end
   
 
-# Your MyLinkedList object will be instantiated and called as such:
-# obj = MyLinkedList.new()
-# param_1 = obj.get(index)
-# obj.add_at_head(val)
-# obj.add_at_tail(val)
-# obj.add_at_index(index, val)
-# obj.delete_at_index(index)
+# doubly link list
+class MyLinkedList
+
+  =begin
+      Initialize your data structure here.
+  =end
+      def initialize()
+          @size = 0
+          @head = nil
+          @tail = nil
+          @vals = []
+      end
+  
+  
+  =begin
+      Get the value of the index-th node in the linked list. If the index is invalid, return -1.
+      :type index: Integer
+      :rtype: Integer
+  =end
+      def get(index)
+         return -1 if index < 0 || index >= @size
+          
+          node = get_node(index)
+          if node
+              node.val
+          else
+              -1
+          end
+      end
+  
+  
+  =begin
+      Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list.
+      :type val: Integer
+      :rtype: Void
+  =end
+      def add_at_head(val)
+          cur_node = Node.new(val)
+          cur_node.next = @head
+          if @head
+              cur_node.next.prev = cur_node
+          end
+      
+          @head = cur_node
+          @tail = cur_node unless @tail
+          @size += 1
+      end
+  
+  
+  =begin
+      Append a node of value val to the last element of the linked list.
+      :type val: Integer
+      :rtype: Void
+  =end
+      def add_at_tail(val)
+          cur_node = Node.new(val)
+          
+          if @tail
+              cur_node.prev = @tail
+              @tail.next = cur_node
+              @tail = cur_node
+          else
+              # @head = cur_node
+              @head = cur_node
+              @tail = cur_node
+          end
+          
+          @size += 1
+      end
+  
+  
+  =begin
+      Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted.
+      :type index: Integer
+      :type val: Integer
+      :rtype: Void
+  =end
+      def add_at_index(index, val)
+          return if index > @size || index < 0
+          
+          cur_node = Node.new(val)
+          
+          if index == @size
+              add_at_tail(val)
+          elsif index == 0
+              add_at_head(val)
+          else
+              prev_node = get_node(index)
+              
+              puts "#{prev_node.val}, #{cur_node.val}"
+              
+              cur_node.prev = prev_node.prev
+              cur_node.next = prev_node
+              
+              prev_node.prev.next = cur_node if prev_node.prev
+              prev_node.prev = cur_node
+          
+              @size += 1
+          end
+          
+      end
+  
+  
+  =begin
+      Delete the index-th node in the linked list, if the index is valid.
+      :type index: Integer
+      :rtype: Void
+  =end
+      def delete_at_index(index)
+          return if index < 0 || index >= @size
+          
+          if index == @size -1 
+              delete_tail
+          elsif index == 0
+              delete_head
+          else
+              cur_node = get_node(index)
+              cur_node.prev.next = cur_node.next
+              cur_node.next.prev = cur_node.prev
+              @size -= 1
+          end
+      end
+  
+  
+  #  get node from index
+      def get_node(index)
+          return nil if index < 0 || index >= @size
+          
+          if index == 0
+              @head
+          elsif index == @size - 1
+              @tail
+          else
+              cur_node = @head
+              while index > 0
+                  cur_node = cur_node.next
+                  index -= 1
+              end
+              cur_node
+          end
+      end
+          
+  #   remove tail
+      def delete_tail
+          # if only one node exist
+          if @size == 1
+              @head = nil
+              @tail = nil
+          else
+              @tail.prev.next = @tail.next
+              @tail = @tail.prev
+          end
+          
+          @size -= 1
+      end
+          
+      #   remove head
+      def delete_head
+          @head = @head.next
+          @head.prev = nil
+          @size -= 1
+      end
+  end
+  
+  class Node
+      attr_accessor :val, :next, :prev
+      
+      def initialize(val)
+          self.val = val
+          self.next = nil
+          self.prev = nil
+      end
+  end
+  
+  # Your MyLinkedList object will be instantiated and called as such:
+  # obj = MyLinkedList.new()
+  # param_1 = obj.get(index)
+  # obj.add_at_head(val)
+  # obj.add_at_tail(val)
+  # obj.add_at_index(index, val)
+  # obj.delete_at_index(index)

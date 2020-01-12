060052f48086770a5c77efab64b53b2b5f072de1
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2020-01-12 10:15:37 +0600

    Update leetcode problem 217

--- leetcode-solutions/ruby/217. Contains Duplicate.rb
+++ leetcode-solutions/ruby/217. Contains Duplicate.rb
@@ -3,3 +3,16 @@
 def contains_duplicate(nums)
     nums.each_with_object(Hash.new(0).merge dup: []){|n, h| h[:dup] << n if (h[n] += 1) >= 2}[:dup].size > 0
 end
+
+# another solution
+# @param {Integer[]} nums
+# @return {Boolean}
+def contains_duplicate(nums)
+  m = {}
+  nums.each do |n|
+      return true if m[n]
+      m[n] = true
+  end
+  
+  false
+end

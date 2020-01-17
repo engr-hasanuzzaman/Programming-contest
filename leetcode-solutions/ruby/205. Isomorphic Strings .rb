f8d7fd5ec1c814d2197f55b960a20c2cdbbe60d2
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2020-01-17 08:56:14 +0600

    Update solution for problem 205

--- leetcode-solutions/ruby/205. Isomorphic Strings .rb
+++ leetcode-solutions/ruby/205. Isomorphic Strings .rb
@@ -19,3 +19,21 @@ def is_isomorphic(s, t)
     
     return true
 end
+
+# updated solution using two array
+# @param {String} s
+# @param {String} t
+# @return {Boolean}
+def is_isomorphic(s, t)
+    sa = []
+    ta = []
+    
+    s.size.times do |i|
+        return false if sa[s[i].ord] != ta[t[i].ord]
+        
+        sa[s[i].ord] = i 
+        ta[t[i].ord] = i
+    end
+    
+    return true
+end

256d9babcae6401704fa87f64628019e1e2bf82a
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2020-03-02 22:53:46 +0600

    Solve problem 1342

--- leetcode-solutions/ruby/1342. Number of Steps to Reduce a Number to Zero.rb
+++ leetcode-solutions/ruby/1342. Number of Steps to Reduce a Number to Zero.rb
@@ -17,3 +17,22 @@ def number_of_steps (num)
   
   count
 end
+
+# ----------- solve problem using bit manupulation ------
+
+# @param {Integer} num
+# @return {Integer}
+# if bit is 1 then we need to sub 1 then div so need 2 step otherwith one step
+# except leftmost 1 that only need one step substruct
+def number_of_steps (num)
+  return num if num <= 1
+  
+  count = -1
+  b_str = num.to_s(2)
+  
+  b_str.each_char do |b|
+      count += b == '0' ? 1 : 2
+  end
+  
+  count
+end

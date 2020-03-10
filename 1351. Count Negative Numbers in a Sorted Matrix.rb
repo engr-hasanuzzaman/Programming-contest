f0fec4dd0b62da69b8775e231c0b35a64741a145
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2020-03-10 23:55:10 +0600

    Simple solution for problem 1351

---
+++ 1351. Count Negative Numbers in a Sorted Matrix.rb
@@ -0,0 +1,18 @@
+# https://leetcode.com/problems/count-negative-numbers-in-a-sorted-matrix/
+
+# @param {Integer[][]} grid
+# @return {Integer}
+def count_negatives(grid)
+  return 0 if grid.size == 0
+  
+  r_size = grid.size - 1
+  c_size = grid.first.size - 1
+  
+  r = 0
+  c = c_size
+  
+  while r < grid.size && c >= 0
+      if grid[i][j] < 0 && grid[i][]
+  end
+      
+end

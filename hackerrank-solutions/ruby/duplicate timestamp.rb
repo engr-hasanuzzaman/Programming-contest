97bca9e823922718a3c30fcf1c542dab3389b407
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2020-05-23 11:41:19 +0600

    Update solution code

--- hackerrank-solutions/ruby/duplicate timestamp.rb
+++ hackerrank-solutions/ruby/duplicate timestamp.rb
@@ -1,23 +1,24 @@
 require 'set'
 
-def parse_date_from_log(l_data)
-  str_date = l_data.split(' - - [')[1].split('] ').first.split(' -').first
-end
-
-input_file_path = File.join(File.dirname(__FILE__), 'input.txt')
-output_file_path = File.join(File.dirname(__FILE__), 'req_input.txt')
+# read the string filename
+filename = gets.strip
+input_file_path = File.join(File.dirname(__FILE__), filename)
+output_file_path = File.join(File.dirname(__FILE__), "req_#{filename}")
 duplicate_timestamps = Set.new()
 last_timestamp = ''
 
-File.foreach(input_file_path) do |log| 
-  timestamp = parse_date_from_log(log)
+File.foreach(input_file_path) do |log|
+  # parse timestamp from log
+  timestamp = log.split(' - - [')[1].split('] ').first.split(' -').first
+
   if last_timestamp == timestamp
     duplicate_timestamps.add(timestamp)
   else
     last_timestamp = timestamp
   end
 end
-  
+
+# write duplicate timestamp to file
 File.open(output_file_path, "w+") do |f|
   f.puts(duplicate_timestamps.to_a)
 end

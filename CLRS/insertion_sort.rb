ba5bbafe4b68d8d077c17549bb7b9bc41ff67972
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2020-12-17 00:19:54 +0600

    Complete the insertion sort in ruby

--- CLRS/insertion_sort.rb
+++ CLRS/insertion_sort.rb
@@ -1,4 +1,15 @@
 def insertion_sort(a)
-    ans = []
-    a.size.times do |
+    1.upto(a.size - 1) do |j|
+        key = a[j]
+        i = j - 1
+        while i >= 0 && a[i] > key
+            a[i+1] = a[i]
+            i -= 1
+        end 
+        a[i+1] = key
+    end
+
+    a
 end
+
+p insertion_sort([1,10,7,4,7,2,-2,-5,0,9090,12,-76])

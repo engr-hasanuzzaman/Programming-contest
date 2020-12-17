7ec6caafd3b9b2c15bc2c4fe8d8462e6ca08014a
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2020-12-18 00:25:16 +0600

    Complete the implementation of merge sort

--- CLRS/merge_sort.rb
+++ CLRS/merge_sort.rb
@@ -1,10 +1,16 @@
 =begin
 // algorithm
 =end
-def merge_sort(q, p, r)
+def merge_sort(a, p, r)
+    return unless p < r
+
+    q = ((r + p)/2).floor
+    merge_sort(a, p, q)
+    merge_sort(a,q+1, r)
+    merger(a, p, q, r)
 end
 
-def merge(a, p, q, r)
+def merger(a, p, q, r)
     left_array = []
     right_array = []
     p.upto(q) do |i|
@@ -14,6 +20,20 @@ def merge(a, p, q, r)
     (q+1).upto(r) do |i|
         right_array << a[i]
     end
-
-    
+    left_array << Float::MAX
+    right_array << Float::MAX
+    i = 0
+    j = 0
+    p.upto(r) do |k|
+        if left_array[i] <= right_array[j]
+            a[k] = left_array[i]
+            i += 1
+        else
+            a[k] = right_array[j]
+            j += 1
+        end
+    end
 end
+a = [1,3,2, 45, 2, -2, 98, 3, 0, 2, -21]
+merge_sort(a, 0, a.size - 1)
+p a

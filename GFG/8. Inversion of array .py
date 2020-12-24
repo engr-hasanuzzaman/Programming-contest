75b8a1335e9804a6e384328c34ba1f4654d94aaf
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2020-12-25 02:03:31 +0600

    Update the note info

--- GFG/8. Inversion of array .py
+++ GFG/8. Inversion of array .py
@@ -3,6 +3,7 @@ Given an array of integers. Find the Inversion Count in the array.
 
 Inversion Count: For an array, inversion count indicates how far (or close) the array is from being sorted. If array is already sorted then the inversion count is 0. If an array is sorted in the reverse order then the inversion count is the maximum. 
 Formally, two elements a[i] and a[j] form an inversion if a[i] > a[j] and i < j.
+Since, we have to complete in O(nlgn), I am using merge sort and counting how many swap needed to sort
 '''
 #User function Template for python3
 import math

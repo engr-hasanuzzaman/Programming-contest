aceb1e478c963be82b3c5343355db5827943bfbc
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2020-12-25 01:54:04 +0600

    Start solving new problem(work in progress)

---
+++ GFG/8. Inversion of array .py
@@ -0,0 +1,46 @@
+'''
+Given an array of integers. Find the Inversion Count in the array. 
+
+Inversion Count: For an array, inversion count indicates how far (or close) the array is from being sorted. If array is already sorted then the inversion count is 0. If an array is sorted in the reverse order then the inversion count is the maximum. 
+Formally, two elements a[i] and a[j] form an inversion if a[i] > a[j] and i < j.
+'''
+#User function Template for python3
+import math
+
+# arr[]: Input Array
+# N : Size of the Array arr[]
+MAX_INPUT_SIZE = 10**18 + 1
+def merger(arr, p, q, r):
+    swap_count = 0
+    left_array = arr[p:q+1]
+    right_array = arr[q+1:r+1]
+    # print("{} {}".format(left_array, right_array))
+    left_array.append(MAX_INPUT_SIZE)
+    right_array.append(MAX_INPUT_SIZE)
+    i = 0
+    j = 0
+    for index in range(p, r+1):
+        if left_array[i] > right_array[j]:
+            arr[index] = right_array[j]
+            j += 1
+            swap_count += 1
+        else:
+            arr[index] = left_array[i]
+            i += 1
+            if right_array[j] == MAX_INPUT_SIZE:
+                swap_count += 1
+    print("sc {},  = {}, {}, {} ".format(swap_count, left_array, right_array, arr))
+    return swap_count
+
+def merge_sort(arr, p, r):
+    if p == r:
+        return 0
+    
+    q = (p+r)//2
+    l_count = merge_sort(arr, p, q)
+    r_count = merge_sort(arr, q+1, r)
+    m_count = merger(arr, p, q, r)
+    return l_count + r_count + m_count
+def inversionCount(a,n):
+    # Your Code Here
+    return merge_sort(a, 0, n-1)

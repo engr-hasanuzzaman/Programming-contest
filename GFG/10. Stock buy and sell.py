fa5e24c4ecd92a9dbe4d48d5e5bef75737ca87b0
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2020-12-26 02:14:23 +0600

    Complete the stock buy and sell problem

--- GFG/10. Stock buy and sell.py
+++ GFG/10. Stock buy and sell.py
@@ -1,15 +1,23 @@
+'''
+The cost of stock on each day is given in an array A[] of size N. 
+Find all the days on which you buy and sell the stock so that
+in between those days your profit is maximum.
+'''
+
 class Solution:
 	def stockBuySell(self, A, n):
 		#code here
-		start_day = 0
+		buy_day = 0
 		ans = []
-		current_price = A[0]
+		previous_price = A[0]
 		for i in range(1,n):
-		    if A[i] <= current_price:
-		        if start_day != i - 1 and i - 1 > start_day:
-		            ans.append([start_day, i-1])
-		        start_day = i 
-		    current_price = A[i]
-		if start_day < n - 1:
-		    ans.append([start_day, n - 1])
+            # if buy price & currenc price same need to change buy_date
+            # if current price is less then pre_price calculate buy & sell
+		    if A[i] < previous_price or A[i] == A[buy_day]:
+		        if buy_day != i - 1 and i - 1 > buy_day:
+		            ans.append([buy_day, i-1])
+		        buy_day = i  # change buy day
+		    previous_price = A[i]
+		if buy_day < n - 1:
+		    ans.append([buy_day, n - 1])
 		return ans
--- bomber.rb
+++ bomber.rb
@@ -1,18 +1,17 @@
-def max_val(arr)
+def max_val(arr, k)
     # track is there bomb on row, col
-    r_size = arr.size - 1
-    c_size = arr.first.size - 1
+    r_size = arr.size
+    c_size = arr.first.size
     b_matrix = get_m_matrix(arr, r_size, c_size)
-    puts "b_matrix is #{b_matrix}"
+    s_cells = get_non_safe_pos(arr, b_matrix, r_size, c_size)
 end
 
 def get_m_matrix(arr, r_size, c_size)
-    b_matrix = Array.new(r_size+1){Array.new(c_size+1){nil}}
-    # puts "i m #{b_matrix}"
+    b_matrix = Array.new(r_size){Array.new(c_size){nil}}
     r_size.times do |i|
         c_size.times do |j|
             if arr[i][j] == -1
-                b_matrix[i] =  Array.new(c_size+1){-1}
+                b_matrix[i] =  Array.new(c_size){-1}
                 r_size.times do |k|
                     b_matrix[k][j] = -1
                 end
@@ -21,6 +20,19 @@ def get_m_matrix(arr, r_size, c_size)
     end
     b_matrix
 end
+
+# original matrix, m indicating m b_m
+def get_non_safe_pos(m, b_m, r_size, c_size)
+    cells = []
+    r_size.times do |i|
+        c_size.times do |j|
+            if m[i][j] != -1 && b_m[i][j] == -1
+                cells << [m[i][j], i, j]
+            end
+        end
+    end
+    cells.sort_by{|e| -e.first}
+end
 #def mark_bomb_row_col(i, j)
 #    arr[i].
 #end
@@ -28,6 +40,6 @@ end
 input = [
     [-1, 8, -1, 3, -1],
     [5, 3, 2, 4, 3],
-    [2, 2, 4, 5, 2]
+    [3, 2, 4, 5, 2]
 ]
 max_val(input)

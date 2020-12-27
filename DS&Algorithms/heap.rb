2377f792f338058a21a17034bf5199c75f198a1d
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2020-12-27 23:38:48 +0600

    Start developing hepify

--- DS&Algorithms/heap.rb
+++ DS&Algorithms/heap.rb
@@ -1,11 +1,33 @@
 # array => heap
-def heapify(arr)
+# normal procedure that take nlogn time but actual heapify take logn time (last to first)
+def make_heap(arr)
     # h_size is current h_size
     1.upto(arr.size-1) do |h_size|
         add_element(arr, h_size, arr[h_size])
     end
 end
 
+def heapify(arr)
+    max_index = arr.size
+    max_index.downto(0) do |i|
+        if left(i) > max_index && right(i) > max_index
+            next
+        elsif left(i) < max_index && right(i) < max_index
+            if arr[left(i)] <= arr[i] && right(i) <= arr[i]
+                next
+            else
+                if arr[left(i)] > arr[right(i)]
+                    arr[left(i)], arr[i] = arr[i], arr[left(i)]
+                else
+                    arr[right(i)], arr[i] = arr[i], arr[right(i)]
+                end
+            end
+            arr[left(i)], arr[i] = arr[i], arr[left(i)]
+        else
+
+        end
+    end
+end
 # h_size is current heap size
 def add_element(heap, h_size, element)
     e_index = h_size
@@ -32,5 +54,5 @@ end
 
 # testing areasize
 input = [4, 10, 3, 5, 1]
-heapify(input)
+make_heap(input)
 p input

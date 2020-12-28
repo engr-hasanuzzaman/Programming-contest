156960220d74daaf9872dc2e7f8660b55afefdb8
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2020-12-29 00:18:25 +0600

    Solve leetcode problem using python

--- DS&Algorithms/std_heap.py
+++ DS&Algorithms/std_heap.py
@@ -1,5 +1,5 @@
 # in-defalut heapq is min heap. If we need max heap we can mul each element with -1 
-from heapq import heapify, heappop, heappush
+from heapq import heapify, heappop, heappush, heapreplace
 hp = [19,4, 4,13,20,2]
 heapify(hp)
 print("after heapify", hp)

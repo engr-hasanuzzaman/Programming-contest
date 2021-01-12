8d4077a9c67f22e6ea7941218924b194b1199de0
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2021-01-13 01:02:16 +0600

    Add comment

--- GFG/Minimum number of Coins.py
+++ GFG/Minimum number of Coins.py
@@ -1,3 +1,6 @@
+# this solution will not work for all the coins 
+# ex. 9, 6, 5, 1 and for 11 it will return 3: 9 + 1 + 1 but optimal one is 2: 5 + 6
+# for general solution we have to compute all the possible solution and pick optimal one
 class Solution:
     def minPartition(self, N):
         coins = [2000, 500, 200, 100, 50, 20, 10, 5, 2, 1]

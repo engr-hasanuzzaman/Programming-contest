6111e40202a89f421bac1c1b4e0fc08fe697c2d2
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2021-01-30 00:06:14 +0600

    add comment to gfg solution

--- GFG/Smallest power of 2 greater than or equal to n.py
+++ GFG/Smallest power of 2 greater than or equal to n.py
@@ -1,11 +1,16 @@
 class Solution:
     def nearestPowerOf2(self,N):
+        # if original N is power of tow
         if N & (N - 1) == 0:
             return N
         
-        p = N
-        while p & (p-1) != 0:
-            print("the p is {}", p)
-            p = p - (p & -p)
+        # if not power of two
+        while N & (N-1) != 0:
+            # (N & -N) extract lower set bit means if N == 1100
+            # (N & -N) will be = 100, that leads to N - (N & -N) -> 1100 - 100 = 1000
+            # in this process we will extract lower set bit untill only one set bit present
+            # that is lower number power of two of the original N 
+            N = N - (N & -N)
         
-        return 2*p
+        # return double of lower power of original N
+        return 2*N

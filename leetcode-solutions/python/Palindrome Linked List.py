33439a2dd9e0e7fe13168deb022228f2853ad45e
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2021-04-01 23:35:03 +0600

    Add description to the solution

--- leetcode-solutions/python/Palindrome Linked List.py
+++ leetcode-solutions/python/Palindrome Linked List.py
@@ -1,8 +1,14 @@
+# https://leetcode.com/problems/palindrome-linked-list/
+
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+'''
+Use recursion to go the end of the link, then compare with the head
+If both have same value, returen true and update the head to next value
+'''
 class Solution:
     def isPalindrome(self, head: ListNode) -> bool:
         self.head = head

bfd65979986f569523ec142826cdf947d6a390f1
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2021-04-03 23:53:25 +0600

    Solution in python

--- leetcode-solutions/python/474. Ones and Zeroes.py
+++ leetcode-solutions/python/474. Ones and Zeroes.py
@@ -1,1 +1,14 @@
 # https://leetcode.com/problems/ones-and-zeroes/
+class Solution:
+    def findMaxForm(self, strs, m, n):
+        xy = [[s.count("0"), s.count("1")] for s in strs]
+
+        @lru_cache(None)
+        
+        def dp(mm, nn, kk):
+            if mm < 0 or nn < 0: return -float("inf")
+            if kk == len(strs): return 0
+            x, y = xy[kk]
+            return max(1 + dp(mm-x, nn-y, kk + 1), dp(mm, nn, kk + 1))
+        
+        return dp(m, n, 0)

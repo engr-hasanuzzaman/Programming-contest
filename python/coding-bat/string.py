00f969d888fafccc96803e604142694eecec77fe
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2021-10-25 19:45:58 +0600

    New problem with solution

--- python/coding-bat/string.py
+++ python/coding-bat/string.py
@@ -25,3 +25,16 @@ first_two('ab') → 'ab
 '''
 def first_two(str):
   return str[:2]
+
+'''
+Q. Given 2 strings, a and b, return a string of the form short+long+short, with the shorter string on the outside and the longer string on the inside. The strings will not be the same length, but they may be empty (length 0).
+
+combo_string('Hello', 'hi') → 'hiHellohi'
+combo_string('hi', 'Hello') → 'hiHellohi'
+combo_string('aaa', 'b') → 'baaab'
+'''
+def combo_string(a, b):
+  if(len(a) > len(b)):
+    return b + a + b
+  else:
+    return a + b + a

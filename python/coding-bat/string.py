60aba3af7dde2c67e1a7492741dbdd9a583e0f39
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2021-10-25 19:57:29 +0600

    New problem with solution

--- python/coding-bat/string.py
+++ python/coding-bat/string.py
@@ -48,3 +48,13 @@ non_start('shotl', 'java') → 'hotlava'
 '''
 def non_start(a, b):
   return a[1:] + b[1:]  
+
+'''
+Q. Given a string, return a "rotated left 2" version where the first 2 chars are moved to the end. The string length will be at least 2.
+
+left2('Hello') → 'lloHe'
+left2('java') → 'vaja'
+left2('Hi') → 'Hi'
+'''
+def left2(str):
+  return str[2:] + str[:2]

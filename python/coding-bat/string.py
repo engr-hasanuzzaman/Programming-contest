c373a4bb551f5db91e66ebe4296e790335f4a002
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2021-10-25 19:48:38 +0600

    New problem with solution

--- python/coding-bat/string.py
+++ python/coding-bat/string.py
@@ -38,3 +38,13 @@ def combo_string(a, b):
     return b + a + b
   else:
     return a + b + a
+
+'''
+Q. Given 2 strings, return their concatenation, except omit the first char of each. The strings will be at least length 1.
+
+non_start('Hello', 'There') → 'ellohere'
+non_start('java', 'code') → 'avaode'
+non_start('shotl', 'java') → 'hotlava'
+'''
+def non_start(a, b):
+  return a[1:] + b[1:]  

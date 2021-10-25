4cadd72920fd716eb975ffa421c4e1d4d4de9caa
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2021-10-25 19:44:03 +0600

    New problem with solution

--- python/coding-bat/string.py
+++ python/coding-bat/string.py
@@ -7,8 +7,7 @@ def make_out_word(out, word):
   return out[:2] + word + out[2:]
 
 '''
-Q:
-Given a string, return a new string made of 3 copies of the last 2 chars of the original string. The string length will be at least 2.
+Q: Given a string, return a new string made of 3 copies of the last 2 chars of the original string. The string length will be at least 2.
 
 extra_end('Hello') → 'lololo'
 extra_end('ab') → 'ababab'
@@ -16,3 +15,13 @@ extra_end('Hi') → 'HiHiHi'
 '''
 def extra_end(str):
   return str[-2:] * 3
+
+'''
+Q: Given a string, return the string made of its first two chars, so the String "Hello" yields "He". If the string is shorter than length 2, return whatever there is, so "X" yields "X", and the empty string "" yields the empty string "".
+
+first_two('Hello') → 'He'
+first_two('abcdefg') → 'ab'
+first_two('ab') → 'ab
+'''
+def first_two(str):
+  return str[:2]

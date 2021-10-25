8e01d2558d95622888acae0a01f911bf59594574
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2021-10-25 19:40:47 +0600

    Practice python for becoming more comportable

---
+++ python/coding-bat/string.py
@@ -0,0 +1,8 @@
+'''
+make_out_word('<<>>', 'Yay') → '<<Yay>>'
+make_out_word('<<>>', 'WooHoo') → '<<WooHoo>>'
+make_out_word('[[]]', 'word') → '[[word]]'
+'''
+def make_out_word(out, word):
+  return out[:2] + word + out[2:]
+

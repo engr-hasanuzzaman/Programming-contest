7693fb47b7012abb927286e9e8834e6c1a27449e
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2021-11-03 00:54:08 +0600

    Binary exponential with itiretive process

--- learning/binary exp.py
+++ learning/binary exp.py
@@ -16,3 +16,19 @@ print('with input 2, 4 ', pow(2, 4) == 16)
 print('with input 2, 3 ', pow(2, 3) == 8)
 print('with input 2, 5 ', pow(2, 5) == 32)
 print('with input 3, 8 ', pow(3, 13) == 1594323)
+
+def powItaretive(a: int, b: int)->int:
+    res = 1
+    while b != 0:
+        if b % 2:
+            res = res * a
+        # every time it will be double
+        a = a * a
+        b = b >> 1
+    return res
+
+    # test
+print('with input 2, 4 ', powItaretive(2, 4) == 16)
+print('with input 2, 3 ', powItaretive(2, 3) == 8)
+print('with input 2, 5 ', powItaretive(2, 5) == 32)
+print('with input 3, 8 ', powItaretive(3, 13) == 1594323)

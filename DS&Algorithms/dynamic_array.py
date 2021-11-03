115a5935327685a66f98bba8df2e7ef79b038553
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2021-11-03 10:22:39 +0600

    Dynamic array with add & remove method

--- DS&Algorithms/dynamic_array.py
+++ DS&Algorithms/dynamic_array.py
@@ -2,7 +2,7 @@ import unittest
 
 class DArray:
     def __init__(self, size) -> None:
-        self.array = [None] * size
+        self.__array = [None] * size
         self.length = 0
         self.capacity = size
     
@@ -12,11 +12,26 @@ class DArray:
             self.capacity *= 2
             newArr = [None] * self.capacity
             for i in range(self.length):
-                newArr[i] = self.array[i]
-            self.array = newArr
-        self.array[self.length] = x
+                newArr[i] = self.__array[i]
+            self.__array = newArr
+        self.__array[self.length] = x
         self.length += 1
 
+    def remove(self, element):
+        i, j, size = 0, 0, self.length
+        while i < size:
+            # if expected elemen, skip copying to new array
+            if self.__array[i] == element:
+                i += 1
+                self.length -= 1
+            else:
+                self.__array[j] = self.__array[i]
+                i += 1
+                j += 1
+        return element
+
+    def elements(self):
+        return self.__array[:self.length]
 
 class Testing(unittest.TestCase):
     def test_new_array_len(self):
@@ -24,6 +39,17 @@ class Testing(unittest.TestCase):
         self.assertEqual(arr.length, 0)
         arr.add(1)
         self.assertEqual(arr.length, 1)
+
+    def test_remove(self):
+        arr = DArray(4)
+        arr.add(1)
+        arr.add(2)
+        arr.add(1)
+        self.assertEqual(arr.length, 3)
+        self.assertEqual(arr.elements(), [1,2,1])
+        self.assertEqual(arr.remove(1), 1)
+        self.assertEqual(arr.length, 1)
+        self.assertEqual(arr.elements(), [2])
 
 if __name__ == '__main__':
     unittest.main()

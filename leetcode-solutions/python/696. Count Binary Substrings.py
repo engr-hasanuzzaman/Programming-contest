7432aa20f6c4b3f7b3487c8765452794b5c34943
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2021-11-16 23:25:02 +0600

    Accepted solution for the problem

--- leetcode-solutions/python/696. Count Binary Substrings.py
+++ leetcode-solutions/python/696. Count Binary Substrings.py
@@ -35,3 +35,31 @@ class Solution:
                 return False
 
         return True
+
+# accepted solution
+'''
+Let's try to count the number of valid binary strings between groups[i] and groups[i+1].
+If we have groups[i] = 2, groups[i+1] = 3, then it represents either "00111" or "11000".
+We clearly can make min(groups[i], groups[i+1]) valid binary strings within this string.
+Because the binary digits to the left or right of this string must change at the boundary,
+our answer can never be larger.
+'''
+class Solution:
+    def countBinarySubstrings(self, s: str) -> int:
+        group = []
+        last_char = s[0]
+        count = 0
+        for i in range(len(s)):
+            if s[i] == last_char:
+                count += 1
+            else:
+                group.append(count)
+                last_char = s[i]
+                count = 1
+        # insert the last char count
+        group.append(count)
+        ans = 0
+        # ex: 000111 -> 0011 -> 01 so min size of the two char count
+        for i in range(len(group) - 1):
+            ans = ans + min(group[i], group[i+1])
+        return ans

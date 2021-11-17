cd1160905f998254d34288fea8887f41a2cb42ed
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2021-11-18 00:31:08 +0600

    New leetcode solution in python

---
+++ leetcode-solutions/python/1137. N-th Tribonacci Number.py
@@ -0,0 +1,8 @@
+# https://leetcode.com/problems/n-th-tribonacci-number/
+
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        dp = [0, 1, 1]
+        for i in range(3, n+1):
+            dp.append(dp[i-1] + dp[i-2] + dp[i-3])
+        return dp[n]

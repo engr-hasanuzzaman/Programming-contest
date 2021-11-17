a854057c2cc2a8490b9d1072667f39e3f7d42db4
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2021-11-18 00:10:11 +0600

    Solution with python

---
+++ leetcode-solutions/python/509. Fibonacci Number.py
@@ -0,0 +1,31 @@
+# https://leetcode.com/problems/fibonacci-number/
+
+# dynamic programming
+class Solution:
+    def fib(self, n: int) -> int:
+        memo = {}
+        def findFebo(n):
+            if n <= 1:
+                return n
+            
+            if n in memo:
+                return memo[n]
+            
+            febo = findFebo(n - 1) + findFebo(n - 2)
+            memo[n] = febo
+            return febo
+        return findFebo(n)
+
+# normal solution with const memory
+class Solution:
+    def fib(self, n: int) -> int:
+        if n <= 1:
+            return n
+    
+        f_prev = 0
+        s_prev = 1
+        for i in range(2, n+1):
+            temp = s_prev
+            s_prev = s_prev + f_prev
+            f_prev = temp
+        return s_prev

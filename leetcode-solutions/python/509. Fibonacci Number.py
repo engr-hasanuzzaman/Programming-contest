da1bf76f936fff7bed0dcdc65031b3da0b9cc886
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2021-11-18 00:11:00 +0600

    Solution fibonacci with dynamic programming and short code

--- leetcode-solutions/python/509. Fibonacci Number.py
+++ leetcode-solutions/python/509. Fibonacci Number.py
@@ -16,6 +16,15 @@ class Solution:
             return febo
         return findFebo(n)
 
+# shoter code with dynamic programming
+class Solution:
+    def fib(self, n: int) -> int:
+        dp = [0, 1]
+        
+        for i in range(2, n+1):
+            dp.append(dp[i-1] + dp[i-2])
+        return dp[n]
+
 # normal solution with const memory
 class Solution:
     def fib(self, n: int) -> int:

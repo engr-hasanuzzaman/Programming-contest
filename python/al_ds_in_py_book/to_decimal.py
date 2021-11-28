f9e4c55f9f5ff0048ea2ff7f536823bad0c2da3f
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2021-11-28 23:42:52 +0600

    Write to_decimal converter from other base

---
+++ python/al_ds_in_py_book/to_decimal.py
@@ -0,0 +1,13 @@
+def to_decimal(number, base: int) -> int:
+    multiplier, result = 1, 0
+
+    while number > 0:
+        result += (number % 10) * multiplier
+        multiplier *= base
+        number //= 10
+    return result
+
+if __name__ == '__main__':
+    assert(to_decimal(100, 2) == 4)
+    assert(to_decimal(11, 2) == 3)
+    assert(to_decimal(1001, 2) == 9)

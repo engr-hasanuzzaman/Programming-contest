8abfe6364f3e0489e685d8264341dbba19ef8913
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2022-01-06 00:30:24 +0600

    Solve new leetcode problem using python

--- leetcode-solutions/python/Algorithm-1/189. Rotate Array.py
+++ leetcode-solutions/python/Algorithm-1/189. Rotate Array.py
@@ -27,4 +27,11 @@ class Solution:
                     break
             start += 1
             
-        
+# solution 2: using python
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        k = k % len(nums)
+        nums[:] = nums[-k:] + nums[:-k]

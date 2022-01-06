a0716c20a9ef92600b232531e3d107666dbdfa47
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2022-01-06 18:45:43 +0600

    Solve new leetcode problem using python

--- leetcode-solutions/python/1094. Car Pooling.py
+++ leetcode-solutions/python/1094. Car Pooling.py
@@ -1,5 +1,8 @@
 # https://leetcode.com/problems/car-pooling/
 
+# solution 1
+# create array with max trip length
+# if the max visit be very large the array will be very large and solution will not be very efficient
 class Solution:
     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
         visit = [0] * 1001
@@ -11,6 +14,26 @@ class Solution:
         sum = 0
         for v in visit:
             sum += v
+            if sum > capacity:
+                return False
+        return True
+
+# solution:
+# instead of keeping visit info on array, create a tuple for eache visit info
+# for each visit, add (start, capacity) & (end, -capacity) and sort the array with first element
+# visit each elment and sum the visitors, if any point vigitor number be > capacity return False
+# otherwise return True 
+class Solution:
+    def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
+        visit = []
+        
+        for t in trips:
+            visit.append((t[1], t[0]))
+            visit.append((t[2], -t[0]))
+        visit.sort()
+        sum = 0
+        for v in visit:
+            sum += v[1]
             if sum > capacity:
                 return False
         return True

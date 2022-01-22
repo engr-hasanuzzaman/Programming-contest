a59e6b6880f55d3f4a0a750e81f2fb024a0c08f4
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2022-01-23 00:10:56 +0600

    Add rules for red-black tree

---
+++ DS&Algorithms/red_black_tree.py
@@ -0,0 +1,9 @@
+'''
+Properties of Red-Black tree
+- it has to be BST and self balancing tree
+- every node has to be either red/black
+- every node which is Nil is black
+- root node has to be black
+- red node shoul not be adjacent (black node could be adjacent)
+- the number of black nodes from root to each leaf must have to be the same
+'''

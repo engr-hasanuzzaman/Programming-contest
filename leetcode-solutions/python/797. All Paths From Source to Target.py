2010774b340f33ac888c74f27c541c5dfa337820
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2022-01-25 23:45:47 +0600

    Solve old problem in a new way

--- leetcode-solutions/python/797. All Paths From Source to Target.py
+++ leetcode-solutions/python/797. All Paths From Source to Target.py
@@ -14,3 +14,19 @@ class Solution:
         for i in graph[index]:
             self.bt(graph, i, target, ans, cur_list)
         cur_list.pop()
+
+# iterative solution
+class Solution:
+    def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+        ans = []
+        
+        # dfs with itarative way
+        # [node, path_array]
+        stack = [[0, [0]]]
+        while stack:
+            [node, path] = stack.pop()
+            if node == len(graph) - 1:
+                ans.append(path[:])
+            for neighbor in graph[node]:
+                stack.append([neighbor, path[:] + [neighbor]])
+        return ans

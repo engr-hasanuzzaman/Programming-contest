f71a5390085bfdfba2f38f322d614fed799f604d
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2022-01-26 21:22:19 +0600

    Solve new problem using python

---
+++ leetcode-solutions/python/219. Contains Duplicate II.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/contains-duplicate-ii/
+
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        memo = {}
+        for i, num in enumerate(nums):
+            if num in memo and i - memo[num] <= k:
+                return True
+            else:
+                memo[num] = i
+        return False

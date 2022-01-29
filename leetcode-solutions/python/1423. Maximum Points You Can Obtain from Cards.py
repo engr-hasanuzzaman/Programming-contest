19f848d1729b3569a27cf02467f2f41a61c05898
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2022-01-29 23:33:13 +0600

    Solve new problem using sliding window technique

--- leetcode-solutions/python/1423. Maximum Points You Can Obtain from Cards.py
+++ leetcode-solutions/python/1423. Maximum Points You Can Obtain from Cards.py
@@ -21,3 +21,26 @@ class Solution:
         self.max_sum = 0
         max_point(cardPoints, 0, len(cardPoints) - 1, 0, k)
         return self.max_sum
+
+# solve using sliding window
+# What is asked in this problem: we need to take some numbers and some numbers from the beginning, 
+# such that the sum of therse taken k numbers are maximal. 
+# It is equivalent to take n - k adjacent numbers with minimal sum and then return sum of all numbers minus this value.
+class Solution:
+    def maxScore(self, cardPoints: List[int], k: int) -> int:
+        total_sum = 0
+        min_sum = float('inf')
+        cur_sum = 0
+        start = 0
+        size =  len(cardPoints) - k # sub-array size for which sum is min
+        
+        for index, point in enumerate(cardPoints):
+            total_sum += point
+            cur_sum += point
+            if size > 0 and index >= size - 1:
+                min_sum = min(min_sum, cur_sum)
+                cur_sum -= cardPoints[start]
+                start += 1
+        # print(min_sum, total_sum)
+        if size == 0: return total_sum
+        return total_sum - min_sum

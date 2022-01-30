3ff02043ffd2420cc298ecc452f4fd259840541f
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2022-01-30 20:17:29 +0600

    Solve a problem using differenet technique

--- leetcode-solutions/python/1493. Longest Subarray of 1's After Deleting One Element.py
+++ leetcode-solutions/python/1493. Longest Subarray of 1's After Deleting One Element.py
@@ -28,3 +28,32 @@ class Solution:
         if one_count == size:
             return size - 1
         return max(prefix_counter)
+
+# solution using sliding window technique
+class Solution:
+    def longestSubarray(self, nums: List[int]) -> int:
+        num_remove = 1
+        left = 0
+        max_size = 0
+        cur_one_counter = 0
+        one_counter = 0
+        for i, n in enumerate(nums):
+            if n == 1:
+                cur_one_counter += 1
+                one_counter += 1
+            else:
+                num_remove -= 1
+
+            while num_remove < 0:
+                if nums[left] == 0:
+                    num_remove += 1
+                else:
+                    cur_one_counter -= 1
+                left += 1
+            
+            max_size = max(max_size, cur_one_counter)
+
+        if one_counter == len(nums):
+            return len(nums) - 1
+    
+        return max_size

62c006fa60238d343fd27bf164b9ec39022a11b3
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2022-02-10 00:17:34 +0600

    Add new TLE solution

---
+++ leetcode-solutions/ruby/2120. Execution of All Suffix Instructions Staying in a Grid.rb
@@ -0,0 +1,34 @@
+# https://leetcode.com/problems/execution-of-all-suffix-instructions-staying-in-a-grid/
+
+# Time limit exceed solution n ** 2 solution
+
+# @param {Integer} n
+# @param {Integer[]} start_pos
+# @param {String} s
+# @return {Integer[]}
+def execute_instructions(n, start_pos, s)
+    ans = []
+    start = 0
+    while start < s.size
+        ans << count_moves(n, start_pos[0], start_pos[1], s[start..])
+        start += 1
+    end
+    ans
+end
+
+def count_moves(size, row, col, ins, cur_count = 0)
+    return cur_count if ins.size.zero?
+    moves = {
+        "R" => [0, 1],
+        "L" => [0, -1],
+        "U" => [-1, 0],
+        "D" => [1, 0]
+    }
+    new_row = row + moves[ins[0]][0]
+    new_col = col + moves[ins[0]][1]
+    ins = ins[1..] 
+    if new_row >= size || new_row < 0 || new_col >= size || new_col < 0
+        return cur_count
+    end
+    return count_moves(size, new_row, new_col, ins, cur_count + 1)
+end

6b2c1cd9f70ce3e0b8c98f7443d81017152d0cc8
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2022-02-12 17:58:00 +0600

    Solve problem using different ways

--- leetcode-solutions/ruby/347. Top K Frequent Elements.rb
+++ leetcode-solutions/ruby/347. Top K Frequent Elements.rb
@@ -17,3 +17,8 @@ def top_k_frequent(nums, k)
    end
      processed_input.sort_by{|a| -a.first}.take(k).map{|e| e.last}
  end
+
+#  using sort  by frequency
+def top_k_frequent(nums, k)
+    nums.each_with_object(Hash.new(0)){|n, h| h[n] += 1}.sort_by{|_, val| -val }.take(k).map{|e| e.first}
+end

e60e2b0ad92a387b8c69791231e606d9356c92b6
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2022-02-13 01:03:22 +0600

    Solve old problem with better worse time complexity

--- leetcode-solutions/49. Group Anagrams.rb
+++ leetcode-solutions/49. Group Anagrams.rb
@@ -1,5 +1,6 @@
 # https://leetcode.com/problems/group-anagrams/
 
+# time complexity is O(n.mlogm)
 # @param {String[]} strs
 # @return {String[][]}
 def group_anagrams(strs)
@@ -14,4 +15,24 @@ def group_anagrams(strs)
   end
   
   map.values
+end
+
+# since str contains only lowercase char, we can take frequency as the key
+# now solution is O(n.m) where n number of string and m max size of string
+
+# @param {String[]} strs
+# @return {String[][]}
+def group_anagrams(strs)
+    hash = Hash.new{|h, k| h[k] = []}
+    strs.each_with_object(hash){|s, h| h[frequency(s)] << s}.values
+end
+
+def frequency(str)
+    arr = Array.new(26, 0)
+    str.each_char do |chr|
+        idx = chr.ord - 'a'.ord
+        arr[idx] += 1
+    end
+    
+    arr.join('-')
 end

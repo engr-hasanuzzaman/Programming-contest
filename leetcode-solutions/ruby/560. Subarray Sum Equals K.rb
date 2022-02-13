e435e9def067bd25ba3c6fb09092e85935e2d2c6
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2022-02-13 11:40:17 +0600

    Updated code of the old solution

--- leetcode-solutions/ruby/560. Subarray Sum Equals K.rb
+++ leetcode-solutions/ruby/560. Subarray Sum Equals K.rb
@@ -23,3 +23,24 @@ def subarray_sum(nums, k)
   
   ans
 end
+
+=begin
+The idea behind this approach is as follows: 
+If the cumulative sum(represented by sum[i]sum[i] for sum up to i^{th}ith  index) up to two indices is the same, 
+the sum of the elements lying in between those indices is zero. Extending the same thought further, 
+if the cumulative sum up to two indices, say ii and jj is at a difference of kk i.e. if sum[i] - sum[j] = ksum[i]−sum[j]=k, the sum of elements lying between indices ii and jj is kk.
+=end
+
+def subarray_sum(nums, k)
+    sum_frequency_counter = Hash.new(0)
+    sum_frequency_counter[0] = 1
+    count = 0
+    prefix_sum = 0
+    nums.each do |num|
+        prefix_sum += num
+        count += sum_frequency_counter[prefix_sum - k]
+        sum_frequency_counter[prefix_sum] += 1
+    end
+    
+    return count
+end

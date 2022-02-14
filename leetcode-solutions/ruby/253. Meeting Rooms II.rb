a43e5ef506a3c74d2a56d3c0b8be64f40a6e8325
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2022-02-15 00:42:20 +0600

    Add question for future reference

--- leetcode-solutions/ruby/253. Meeting Rooms II.rb
+++ leetcode-solutions/ruby/253. Meeting Rooms II.rb
@@ -1,4 +1,17 @@
 # https://leetcode.com/problems/meeting-rooms-ii/
+# https://leetcode.com/problems/meeting-rooms-ii/
+=begin
+Given an array of meeting time intervals intervals where intervals[i] = [starti, endi], return the minimum number of conference rooms required.
+
+Example 1:
+
+Input: intervals = [[0,30],[5,10],[15,20]]
+Output: 2
+Example 2:
+
+Input: intervals = [[7,10],[2,4]]
+Output: 1
+=end
 
 require 'algorithms'
 include Containers

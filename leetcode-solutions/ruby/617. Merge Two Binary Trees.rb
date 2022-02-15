288955fd03a6269ba0e38584ade1b18df6a09536
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2022-02-15 23:48:40 +0600

    Different way to solve the problem

--- leetcode-solutions/ruby/617. Merge Two Binary Trees.rb
+++ leetcode-solutions/ruby/617. Merge Two Binary Trees.rb
@@ -12,3 +12,33 @@ def merge_trees(root1, root2)
     
     root1 || root2
 end
+
+# itarative solution
+def merge_trees(root1, root2)
+    if !root1 || !root2
+        return root1 || root2
+    end
+    
+    stack = [[root1, root2]]
+    
+    until stack.empty?
+        node1, node2 = stack.pop
+        if node1.nil?
+            next
+        end
+        
+        node1.val += node2.val
+        if node1.left && node2.left
+            stack.push([node1.left, node2.left])
+        elsif node1.left.nil?
+            node1.left = node2.left
+        end
+        
+        if node1.right && node2.right
+            stack.push([node1.right, node2.right])
+        elsif node1.right.nil?
+            node1.right = node2.right
+        end
+    end
+    root1
+end

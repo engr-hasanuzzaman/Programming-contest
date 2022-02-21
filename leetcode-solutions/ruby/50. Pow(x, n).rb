f45b15352b619803eb226c008e3b8ec840a77135
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2022-02-21 16:53:46 +0600

    Solve problem using diff way

--- leetcode-solutions/ruby/50. Pow(x, n).rb
+++ leetcode-solutions/ruby/50. Pow(x, n).rb
@@ -8,4 +8,28 @@ def my_pow(x, n)
   x**n
 end
 
-# 
+# alternative solution
+# @param {Float} x
+# @param {Integer} n
+# @return {Float}
+def my_pow(x, n)
+  if n < 0
+      n = n.abs
+      x = 1 / x
+  end
+  
+  ans = 1
+  
+  until n.zero?
+      if n % 2 != 0
+          ans = ans * x
+      end
+      
+      n /= 2
+      x *= x
+  end
+  
+  ans
+end
+
+# 2**8 = (2 * 2) * (2 * 2) each etaration we are doubling our x and resetting n by 2

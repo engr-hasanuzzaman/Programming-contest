e7cdf023e5482f485021a077589e980fafb8cf5e
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2022-02-27 00:24:48 +0600

    Solve new problem using ruby

---
+++ leetcode-solutions/ruby/169. Majority Element.rb
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/majority-element/
+
+def majority_element(nums)
+    # Boyer-Moore Voting Algorithm
+   candidate = nil
+   count = 0
+   nums.each do |num|
+       if count.zero?
+           candidate = num
+       end
+       
+       count += 1 if num == candidate
+       count -= 1 if num != candidate
+   end
+   
+   candidate
+end
+
+# since majority element frequency is > size / 2, if we sort the nums size / 2 th element will be
+# the majority element

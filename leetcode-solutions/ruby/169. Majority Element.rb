c5c911fb7c6ea4e36fb2a584ec8faf1fa164cbca
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2022-02-27 00:25:44 +0600

    Solve problem in different ways

--- leetcode-solutions/ruby/169. Majority Element.rb
+++ leetcode-solutions/ruby/169. Majority Element.rb
@@ -22,3 +22,21 @@ def majority_element(nums)
     nums.sort!
     nums[nums.size / 2]
 end
+
+# using hashmap
+def majority_element(nums)
+    e_counter = {}
+    res = nums.first
+    max_count = 0
+    nums.each do |n|
+        if e_counter[n]
+            e_counter[n] += 1
+        else
+            e_counter[n] = 1
+        end
+        
+        if e_counter[n] > nums.size / 2
+            return n
+        end
+    end
+end

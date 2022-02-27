52aae48c6a21546ecb1eb67c455eb16b147f2c27
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2022-02-27 23:45:39 +0600

    Diff solution for problem

--- leetcode-solutions/ruby/300. Longest Increasing Subsequence.rb
+++ leetcode-solutions/ruby/300. Longest Increasing Subsequence.rb
@@ -16,3 +16,23 @@ def length_of_lis(nums)
     
     dp.max
 end
+
+# keep max_size
+# @param {Integer[]} nums
+# @return {Integer}
+def length_of_lis(nums)
+    dp = Array.new(nums.size){1}
+    n = nums.size
+    max_size = 1
+    n.times do |i|
+        0.upto(i-1) do |j|
+            if nums[j] < nums[i]
+                dp[i] = [dp[i], dp[j] + 1].max
+                max_size = [max_size, dp[i]].max
+            end
+        end
+    end
+    
+    max_size
+end
+    

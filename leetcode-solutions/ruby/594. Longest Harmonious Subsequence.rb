66c6241419919a9128ead0db5ead1828ef5cca9d
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2022-02-27 22:59:08 +0600

    Solve problem in different way

--- leetcode-solutions/ruby/594. Longest Harmonious Subsequence.rb
+++ leetcode-solutions/ruby/594. Longest Harmonious Subsequence.rb
@@ -17,3 +17,39 @@ def find_lhs(nums)
     
     max_size
 end
+
+# with constant memory
+# @param {Integer[]} nums
+# @return {Integer}
+def find_lhs(nums)
+    
+    nums.sort!
+    cur_num = nums.first
+    index = 1
+    cur_count = 1
+    max_count = 0
+    while index < nums.size
+        num = nums[index]
+        # same number 
+        if num == cur_num
+            cur_count += 1
+            index += 1
+        else
+            if num == cur_num + 1
+                next_count = 0
+                while index < nums.size && nums[index] == cur_num + 1
+                    index += 1
+                    next_count += 1
+                end
+                max_count = [max_count, cur_count + next_count].max
+                cur_count = next_count
+            else
+                cur_count = 1
+                index += 1
+            end
+            
+            cur_num = num
+        end
+    end
+    max_count
+end

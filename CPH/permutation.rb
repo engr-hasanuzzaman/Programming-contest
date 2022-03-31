2528f9e47510837a17749aec130dc3372c607176
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2022-04-01 00:02:50 +0600

    Code for generating permutation

---
+++ CPH/permutation.rb
@@ -0,0 +1,20 @@
+def permutation(arr, visited = {}, cur_per = [], ans = [])
+  if cur_per.size == arr.size
+    ans << cur_per[0..-1]
+  else
+    arr.each do |num|
+      next if visited[num]
+
+      visited[num] = true
+      cur_per.push num
+      permutation(arr, visited, cur_per, ans)
+
+      visited[num] = false
+      cur_per.pop
+    end
+  end
+
+  ans
+end
+
+puts "-----permutation of 1,2,3 is #{permutation([1, 2, 3])}"

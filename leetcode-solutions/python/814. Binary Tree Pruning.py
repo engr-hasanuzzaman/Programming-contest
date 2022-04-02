64ef6bb9733f256273fad4292754aec772d78d71
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2022-04-02 20:33:27 +0600

    Solve leetcode problem using python

---
+++ leetcode-solutions/python/814. Binary Tree Pruning.py
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/binary-tree-pruning/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def pruneTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return root
+        
+        root.left = self.pruneTree(root.left)
+        root.right = self.pruneTree(root.right)
+        if not root.left and not root.right and root.val == 0:
+            return None
+        
+        return root
+    

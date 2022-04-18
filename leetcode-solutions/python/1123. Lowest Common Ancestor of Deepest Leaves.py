a7d93f8cc5308664d31c3a59b03272c141eccc85
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2022-04-18 06:08:08 +0600

    Solve LCA problem using eulerian path

--- leetcode-solutions/python/1123. Lowest Common Ancestor of Deepest Leaves.py
+++ leetcode-solutions/python/1123. Lowest Common Ancestor of Deepest Leaves.py
@@ -29,3 +29,46 @@ class Solution:
             return node if left and right else left or right
         
         return find_lca(root)
+
+
+# solution using Eulerian path
+class Solution:
+    def lcaDeepestLeaves(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        # e_ -> Eulerian depth/index
+        e_depth = []
+        e_nodes = []
+        last_visit = {}
+        # 1 -> 2 -> 3 ->2 ->
+        def eulerian_tour(root, depth=0):
+            if not root: return
+            visit(root, depth)
+            # root node
+            if not root.left and not root.right: return
+            if root.left:
+                eulerian_tour(root.left, depth+1)
+                visit(root, depth)
+            if root.right:
+                eulerian_tour(root.right, depth+1)
+                visit(root, depth)
+        
+        # update e_depth & e_nodes array
+        # also keep tracking of last visited e_index
+        def visit(node, depth):
+            e_depth.append(depth)
+            e_nodes.append(node)
+            # keep last e-index 
+            last_visit[node.val] = len(e_depth) - 1
+        
+        # eulerian_tour
+        eulerian_tour(root, 0)
+        
+        # get the max depth value
+        max_depth = max(e_depth)
+
+        # nodes' last e-index with max_depth
+        indexs = [last_visit[e_nodes[index].val] for index, depth in enumerate(e_depth) if depth == max_depth]
+        min_index = min(indexs)
+        max_index = max(indexs)
+        
+        # lowers node between indices is the root node
+        return min(e_nodes[min_index:max_index+1], key=lambda n: n.val)

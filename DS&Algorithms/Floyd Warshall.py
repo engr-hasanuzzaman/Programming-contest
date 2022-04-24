5ef808ecc58d4bb3923936a268fce2b6e499f140
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2022-04-24 16:51:14 +0600

    Floyad warshall algorithm

---
+++ DS&Algorithms/Floyd Warshall.py
@@ -0,0 +1,21 @@
+# need to prepare n * b matrix representation of the graph
+# create n * n dp & next (if not want to change input)
+# for exiting weight add dp[i][j] = graph[i][j] and next[i][j] = j if there is a path between i & j
+# update dp
+# for i & j, if dp[i][k] + dp[k][j] < dp[i][j] update dp[i][j] & next[i][j] = next[i][k]
+# dp[i][j] = 
+class Solution:
+    def shortest_distance(self, dp):
+        size = len(dp)
+    for k in range(size):
+        for i in range(size):
+            for j in range(size):
+                i_k = float('inf') if dp[i][k] == -1 else dp[i][k]
+                i_j = float('inf') if dp[i][j] == -1 else dp[i][j]
+                k_j = float('inf') if dp[k][j] == -1 else dp[k][j]
+                val = min(i_j, i_k + k_j)
+                if val == float('inf'):
+                    continue
+                # print("i {i}, j {j}, k {k}", i, j, k, min(dp[i][k] + dp[k][j], dp[i][j]))
+                dp[i][j] = val
+

fb3f58109018bbcf920b8864ab34852eba854e1a
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2022-04-30 13:06:23 +0600

    Refactor code

--- leetcode-solutions/ruby/10. Regular Expression Matching.rb
+++ leetcode-solutions/ruby/10. Regular Expression Matching.rb
@@ -8,7 +8,9 @@ def is_match(s, p)
 
   if (p.size >= 2) && (p[1] == '*')
     # puts("-----s #{s}, p #{p}, #{first_match}")
-    # two carse 1. we can ingore this pattern or keep the same patter to match multiple element
+    # two carse
+    # 1. we can ingore this pattern or 
+    # 2. keep the same patter to match multiple element if curent char matct with pattern
     is_match(s, p[2..-1]) || (first_match && is_match(s[1..-1], p))
   else
     first_match && is_match(s[1..-1], p[1..-1])

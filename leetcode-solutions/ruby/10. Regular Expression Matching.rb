79199ece263d56e6c9720bbadc29aff0fe3743a3
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2022-04-30 13:05:07 +0600

    Solve leetcode problem 10 using recursion

---
+++ leetcode-solutions/ruby/10. Regular Expression Matching.rb
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/regular-expression-matching/
+
+# solve using using recursion
+def is_match(s, p)
+  return s.empty? if p.empty?
+
+  first_match = !!s[0] && ['.', s[0]].include?(p[0])
+
+  if (p.size >= 2) && (p[1] == '*')
+    # puts("-----s #{s}, p #{p}, #{first_match}")
+    # two carse 1. we can ingore this pattern or keep the same patter to match multiple element
+    is_match(s, p[2..-1]) || (first_match && is_match(s[1..-1], p))
+  else
+    first_match && is_match(s[1..-1], p[1..-1])
+  end
+end

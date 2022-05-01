a87531d0e63a6fa8dea1c52ac46d0711204628c5
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2022-05-01 17:37:53 +0600

    Solve leetcode problem 2080 using fenwick tree

--- leetcode-solutions/python/2080. Range Frequency Queries.py
+++ leetcode-solutions/python/2080. Range Frequency Queries.py
@@ -40,3 +40,51 @@ class RangeFreqQuery:
         # bisect_left will return left most postion for insertion
         # if value exist, will return first occurence of value otherwise idx of lower number
         return bisect.bisect_right(idxs, right) - bisect.bisect_left(idxs, left)
+
+# range quiery using fenwick tree
+from collections import defaultdict
+
+class RangeFreqQuery:
+
+    def __init__(self, arr: List[int]):
+        self.ft = FenwickTree(arr)
+
+    def query(self, left: int, right: int, value: int) -> int:
+        return self.ft.range_query(left, right, value)
+
+class FenwickTree:
+    def __init__(self, arr):
+        # first node is dummy node
+        self.tree = [defaultdict(int) for _ in range(len(arr) + 1)]
+
+        # building tree
+        for i in range(1, len(arr) + 1):
+            n = arr[i-1]
+
+            n_idx = i
+            while n_idx < len(self.tree):
+                self.tree[n_idx][n] += 1
+                n_idx = self.next(n_idx)
+    
+    def range_query(self, l, r, value):
+        if l == 0:
+            return self.query(r, value)
+        
+        return self.query(r, value) - self.query(l-1, value)
+
+    def query(self, l, value):
+        # adjust for fenwick tree index
+        l += 1
+
+        freq = 0
+        while l > 0:
+            freq += self.tree[l][value]
+            l = self.parent(l)
+        return freq
+
+    def next(self, idx):
+        return idx + (idx & (-idx))
+    
+    def parent(self, idx):
+        return idx - (idx & (-idx))
+    

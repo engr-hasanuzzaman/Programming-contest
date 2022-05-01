361ff7d805f73054ac1c179af3be70cefbf341e2
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2022-05-01 23:08:12 +0600

    Solve leetcode problem 378 with alternative ways

--- leetcode-solutions/python/378. Kth Smallest Element in a Sorted Matrix.py
+++ leetcode-solutions/python/378. Kth Smallest Element in a Sorted Matrix.py
@@ -24,3 +24,20 @@ class Solution:
 # alternative approach
 # if we consider each row as sorted individual list, then this problem is 
 # similar to merge n sorted list problem
+from heapq import heappop, heappush, heapify,
+class Solution:
+    def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
+        pop = heappop
+        push = heappush
+        # consider n number of sorted list and pick first column
+        heap = [(matrix[i][0], i, 0) for i in range(len(matrix))]
+        heapify(heap)
+        
+        for _ in range(k-1):
+            # pop the min element and pick elemen from the same row
+            _, row, col = pop(heap)
+            # last col
+            if col == len(matrix[0]) - 1: continue
+            # pick next element from the same row as that is potential next smallest value
+            push(heap, (matrix[row][col+1], row, col+1))
+        return heap[0][0]

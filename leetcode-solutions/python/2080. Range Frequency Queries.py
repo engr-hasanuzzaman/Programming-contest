25597d653dc493949b47a15bb1d5c0743d02d088
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2022-05-01 16:01:45 +0600

    Solve new problem using bisect library

--- leetcode-solutions/python/2080. Range Frequency Queries.py
+++ leetcode-solutions/python/2080. Range Frequency Queries.py
@@ -2,6 +2,8 @@ from collections import defaultdict
 from copy import copy
 
 # Memory Limit Exceed solution
+
+
 class RangeFreqQuery:
 
     def __init__(self, arr: List[int]):
@@ -21,3 +23,20 @@ class RangeFreqQuery:
             return r_freq[value]
         else:
             return r_freq[value] - l_freq[value]
+
+
+# using binary search
+class RangeFreqQuery:
+
+    def __init__(self, arr: List[int]):
+        self.frequency = defaultdict(list)
+        for i, n in enumerate(arr):
+            self.frequency[n].append(i)
+
+    def query(self, left: int, right: int, value: int) -> int:
+        idxs = self.frequency[value]
+        # bisect_right return right most position for new insertion that means
+        # if right most index for target is j, it will j+1
+        # bisect_left will return left most postion for insertion
+        # if value exist, will return first occurence of value otherwise idx of lower number
+        return bisect.bisect_right(idxs, right) - bisect.bisect_left(idxs, left)

61e097f7c1548123cc511189953e71b06cb9db83
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2022-05-04 16:48:53 +0600

    Refactor of leetcode problem 48

--- leetcode-solutions/python/Algorithm-1/48. Rotate Image.py
+++ leetcode-solutions/python/Algorithm-1/48. Rotate Image.py
@@ -13,3 +13,21 @@ class Solution:
                 matrix[size - 1 - i][size - 1 - j] = matrix[j][size - 1 - i]
                 matrix[j][size - 1 - i] = matrix[i][j]
                 matrix[i][j] = temp
+
+
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        size = len(matrix)
+        m_row = m_col = size - 1
+        # row will be ceil of half
+        # col will be floor of half
+        for r in range(size // 2 + size % 2):
+            for c in range(size // 2):
+                temp = matrix[r][c]
+                matrix[r][c] = matrix[m_row-c][r]
+                matrix[m_row-c][r] = matrix[m_row-r][m_col-c]
+                matrix[m_row-r][m_col-c] = matrix[c][m_col-r]
+                matrix[c][m_col-r] = temp

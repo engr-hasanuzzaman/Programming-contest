aee96bdc630d6c8ebe7c94a59aa433ce3fc2c215
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2022-05-05 15:54:38 +0600

    Solve leetcode problem 142 using python

---
+++ leetcode-solutions/python/142. Linked List Cycle II.py
@@ -0,0 +1,26 @@
+# https://leetcode.com/problems/linked-list-cycle-ii/
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        fast = slow = head
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+            if fast == slow:
+                break
+
+        if not fast or not fast.next:
+            return None
+
+        slow = head
+        while slow:
+            if slow == fast:
+                return slow
+            slow = slow.next
+            fast = fast.next

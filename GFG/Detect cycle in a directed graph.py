c5474bfabf420d36a68f28e5bd1e55742f5383ca
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2022-05-12 00:43:45 +0600

    Solve GFG problem detect cycle in DG

---
+++ GFG/Detect cycle in a directed graph.py
@@ -0,0 +1,30 @@
+from collections import defaultdict
+
+# TLE solution
+class Solution:
+
+    # Function to detect cycle in a directed graph.
+    def isCyclic(self, V, adj):
+        # print(adj)
+        self.visited = defaultdict(bool)
+        self.overall_visit = defaultdict(bool)
+        self.has_cycle = False
+        for node in range(V):
+            if self.overall_visit[node]:
+                continue
+            self.dfs(adj, node)
+            if self.has_cycle:
+                break
+        return self.has_cycle
+
+    def dfs(self, adj, node):
+        self.overall_visit[node] = True
+        if self.visited[node] or self.has_cycle:
+            # print(self.visited, node)
+            self.has_cycle = True
+            return
+        self.visited[node] = True
+
+        for neighbor in adj[node]:
+            self.dfs(adj, neighbor)
+        self.visited[node] = False

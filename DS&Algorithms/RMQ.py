f4e81548339d08e043ccbefa8075df7173e97bce
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2022-05-15 13:24:55 +0600

    Update notes

--- DS&Algorithms/RMQ.py
+++ DS&Algorithms/RMQ.py
@@ -1,10 +1,12 @@
 import math
 
+
 class Solution:
     def __init__(self, arr) -> None:
         self.elements = arr
-        self.row_size = len(arr) # row size
-        self.k = int(math.log2(self.row_size)) + 1 # col size, each column will hold next level of binary length
+        self.row_size = len(arr)  # row size
+        # col size, each column will hold next level of binary length
+        self.k = int(math.log2(self.row_size)) + 1
         self.min_range_query = [[None] * self.k for _ in range(self.row_size)]
         # print("-------- initial min range ", self.min_range_query)
         self.prepare()
@@ -20,18 +22,26 @@ class Solution:
             st[i][j] = f(st[i][j-1], st[i + (1 << (j - 1))][j - 1]);
     """
     # Reference: https://www.geeksforgeeks.org/sparse-table/?ref=gcse
+    # min_range_query[i][j] will containt min value start from i and length (2^j)
     def prepare(self):
         # initialized singele interval (row wise)
         for i in range(len(self.elements)):
             self.min_range_query[i][0] = self.elements[i]
-    
+
         j = 1
-        while (1 << j) <= self.row_size: 
+        # 2^j is the interval which is <= max array length
+        while (1 << j) <= self.row_size: # or (j <= k where k = math.floor(math.log2(size))) + 1
+            # ex. 2^j = 2, range (0,1), (1,2), (2,3) ....
+            # ex. 2^j = 4, range (0,3), (1,4), (2,5) ....
             i = 0
+            # for 0-based index, < self.row_size
             while (i + (2 ** j) - 1) < self.row_size:
                 # j is range, length of that level
                 # print(f"i {i}, j-1 = {j-1} i + (1 << (j - 1)) {i + (1 << (j - 1))}")
-                self.min_range_query[i][j] = min(self.min_range_query[i][j-1], self.min_range_query[i + (1 << (j - 1))][j-1])
+                self.min_range_query[i][j] = min(
+                    self.min_range_query[i][j-1],
+                    self.min_range_query[i + (1 << (j - 1))][j-1]
+                )
                 i += 1
             j += 1
         # print("--------- after perparing ", self.min_range_query)
@@ -46,8 +56,9 @@ class Solution:
         # and arr[lookup[3][3]],
         # return min(self.min_range_query[L][j], self.min_range_query[R - (1 << j) + 1][j])
         return min(self.min_range_query[L][j], self.min_range_query[R - (2 ** j) + 1][j])
-        
-arr = [1,2,3,4,4,5,5,60,-2,35,6,7,0,1]
+
+
+arr = [1, 2, 3, 4, 4, 5, 5, 60, -2, 35, 6, 7, 0, 1]
 s = Solution(arr)
 print("----MRQ between 0, 6 is ", s.query(0, 6))
 print("----MRQ between 0, 8 is ", s.query(1, 8))

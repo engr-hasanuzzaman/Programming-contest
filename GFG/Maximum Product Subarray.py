34d9bfb20ca0432a8ae63f1299549eb852cae4d9
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2022-05-29 21:15:27 +0600

    Alternative solution

--- GFG/Maximum Product Subarray.py
+++ GFG/Maximum Product Subarray.py
@@ -26,3 +26,40 @@ class Solution:
                 cur_max = temp
             max_product = max(max_product, cur_max, cur_min)
         return max_product
+
+
+# simplified conditions
+class Solution:
+
+    # Function to find maximum
+    # product subarray
+    def maxProduct(self, arr, n):
+
+        # Variables to store maximum and
+        # minimum product till ith index.
+        minVal = arr[0]
+        maxVal = arr[0]
+
+        maxProduct = arr[0]
+
+        for i in range(1, n, 1):
+
+            # When multiplied by -ve number,
+            # maxVal becomes minVal
+            # and minVal becomes maxVal.
+            if (arr[i] < 0):
+                temp = maxVal
+                maxVal = minVal
+                minVal = temp
+
+            # maxVal and minVal stores the
+            # product of subarray ending at arr[i].
+            maxVal = max(arr[i], maxVal * arr[i])
+            minVal = min(arr[i], minVal * arr[i])
+
+            # Max Product of array.
+            maxProduct = max(maxProduct, maxVal)
+
+        # Return maximum product
+        # found in array.
+        return maxProduct

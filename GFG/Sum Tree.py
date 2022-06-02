5eee8db203b15c940f623388b971a0634a3538dc
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2022-06-03 00:51:50 +0600

    Solve sum tree problem

---
+++ GFG/Sum Tree.py
@@ -0,0 +1,32 @@
+'''
+class Node:
+    def __init__(self, val):
+        self.right = None
+        self.data = val
+        self.left = None
+'''
+# your task is to complete this function
+# function should return True is Tree is SumTree else return False
+
+# Flag based solution
+class Solution:
+    def isSumTree(self, root):
+        # flag to indicate sub-node full-fill sum tree property or not
+        # to prevent re-visiting the nodes
+        self.sum_tree = True
+        self.cal_sum(root)
+        return self.sum_tree
+
+    def cal_sum(self, root):
+        if not self.sum_tree or not root:
+            return 0
+
+        if not root.left and not root.right:
+            return root.data
+
+        left_sum = self.cal_sum(root.left)
+        right_sum = self.cal_sum(root.right)
+        if root.data != (left_sum + right_sum):
+            self.sum_tree = False
+
+        return root.data + left_sum + right_sum

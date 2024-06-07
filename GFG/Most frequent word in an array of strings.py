886b0a6d9938b0363d0b901219b06180dd52ef49
Hasan Uz Zaman <hasanuzzaman.sumon@gmail.com>
2024-06-07 01:12:35 +0100

    Update Most frequent word in an array of strings.py

--- GFG/Most frequent word in an array of strings.py
+++ GFG/Most frequent word in an array of strings.py
@@ -26,3 +26,42 @@ class Solution:
         return word_with_max_freq
 
 
+# using trie dataStructure
+from collections import defaultdict
+class Solution:
+    
+    #Function to find most frequent word in an array of strings.
+    def mostFrequentWord(self,arr,n):
+        freq_word = ""
+        cur_max_freq = 0
+        cur_start_index = 0
+        trie = TrieNode()
+        
+        
+        for idx in range(n):
+            word = arr[idx]
+            cur_node = trie
+    
+            for char in word:
+                if cur_node.childrens[char] is None:
+                    cur_node.childrens[char] = TrieNode()
+                cur_node = cur_node.childrens[char]
+            cur_node.word_count += 1
+            if cur_node.word_count == 1:
+                cur_node.first_index = idx
+        
+    
+            if cur_node.word_count > cur_max_freq or (cur_node.word_count == cur_max_freq and cur_node.first_index > cur_start_index):
+                freq_word = word
+                cur_max_freq = cur_node.word_count
+                cur_start_index = cur_node.first_index
+        return freq_word
+    
+
+class TrieNode:
+  def __init__(self):
+      self.childrens = defaultdict(lambda: None)
+      self.word_count = 0
+      self.first_index = 0
+      
+
